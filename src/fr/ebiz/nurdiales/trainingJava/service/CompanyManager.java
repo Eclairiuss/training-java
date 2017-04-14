0a210c990fe6c5c44ea163fa5fd5f9d1d92126c0
Urdiales Nicolas <nurdiales@e-biz.fr>
2017-04-14 17:06:41 +0200

    {MAVEN, REFACTO}

--- src/fr/ebiz/nurdiales/trainingJava/service/CompanyManager.java
+++ src/fr/ebiz/nurdiales/trainingJava/service/CompanyManager.java
@@ -8,66 +8,66 @@ import fr.ebiz.nurdiales.trainingJava.model.Company;
 import fr.ebiz.nurdiales.trainingJava.persistence.CompanyDAO;
 
 public class CompanyManager {
-	// private static final Logger logger =
-	// LoggerFactory.getLogger(ComputerManager.class);
-	private CompanyDAO companyDAO;
+    // private static final Logger logger =
+    // LoggerFactory.getLogger(CompanyManager.class);
+    private CompanyDAO companyDAO;
 
-	public CompanyManager() {
-		this.companyDAO = new CompanyDAO();
-	}
+    public CompanyManager() {
+        this.companyDAO = new CompanyDAO();
+    }
 
-	public Company companyById(int id) throws CompanyDAOException {
-		Company company = null;
-		try {
-			company = companyDAO.companyById(id);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new CompanyDAOException();
-		}
-		return company;
-	}
+    public Company companyById(int id) throws CompanyDAOException {
+        Company company = null;
+        try {
+            company = companyDAO.companyById(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new CompanyDAOException();
+        }
+        return company;
+    }
 
-	public List<Company> allCompanies() throws CompanyDAOException {
-		List<Company> companies = null;
-		try {
-			companies = companyDAO.allCompanies();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new CompanyDAOException();
-		}
-		return companies;
-	}
+    public List<Company> allCompanies() throws CompanyDAOException {
+        List<Company> companies = null;
+        try {
+            companies = companyDAO.allCompanies();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new CompanyDAOException();
+        }
+        return companies;
+    }
 
-	public List<Company> allCompanies(int page, int pageSize) throws CompanyDAOException {
-		List<Company> companies = null;
-		try {
-			companies = companyDAO.allCompanies(page, pageSize);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new CompanyDAOException();
-		}
-		return companies;
-	}
+    public List<Company> allCompanies(int page, int pageSize) throws CompanyDAOException {
+        List<Company> companies = null;
+        try {
+            companies = companyDAO.allCompanies(page, pageSize);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new CompanyDAOException();
+        }
+        return companies;
+    }
 
-	public List<Company> allCompaniesByName(String name) throws CompanyDAOException {
-		List<Company> companies = null;
-		try {
-			companies = companyDAO.allCompaniesByName(name);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new CompanyDAOException();
-		}
-		return companies;
-	}
+    public List<Company> allCompaniesByName(String name) throws CompanyDAOException {
+        List<Company> companies = null;
+        try {
+            companies = companyDAO.allCompaniesByName(name);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new CompanyDAOException();
+        }
+        return companies;
+    }
 
-	public List<Company> allCompaniesByName(String name, int page, int pageSize) throws CompanyDAOException {
-		List<Company> companies = null;
-		try {
-			companies = companyDAO.allCompaniesByName(name, page, pageSize);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new CompanyDAOException();
-		}
-		return companies;
-	}
+    public List<Company> allCompaniesByName(String name, int page, int pageSize) throws CompanyDAOException {
+        List<Company> companies = null;
+        try {
+            companies = companyDAO.allCompaniesByName(name, page, pageSize);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new CompanyDAOException();
+        }
+        return companies;
+    }
 }
