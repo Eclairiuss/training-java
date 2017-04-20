a054a265608e78e4133b3fba3cdd5bacc03f3498
Urdiales Nicolas <nurdiales@e-biz.fr>
2017-04-20 17:00:56 +0200

    {DTO} creation des DTO et création des Mapper

--- src/main/java/fr/ebiz/nurdiales/trainingJava/dto/ComputerDTO.java
+++ src/main/java/fr/ebiz/nurdiales/trainingJava/dto/ComputerDTO.java
@@ -1,5 +1,58 @@
 package fr.ebiz.nurdiales.trainingJava.dto;
 
 public class ComputerDTO {
+    private Integer id;
+    private Integer companyId;
+    private String name;
+    private String introduced;
+    private String discontinued;
+    private String companyName;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIntroduced() {
+        return introduced;
+    }
+
+    public void setIntroduced(String introduced) {
+        this.introduced = introduced;
+    }
+
+    public String getDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(String discontinued) {
+        this.discontinued = discontinued;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 }
---
+++ src/main/java/fr/ebiz/nurdiales/trainingJava/mapper/CompanyMapper.java
@@ -0,0 +1,7 @@
+package fr.ebiz.nurdiales.trainingJava.mapper;
+
+/**
+ * Created by ebiz on 20/04/17.
+ */
+public class CompanyMapper {
+}
