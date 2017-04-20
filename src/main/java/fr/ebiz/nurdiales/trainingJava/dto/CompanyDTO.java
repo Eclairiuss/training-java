6d51a627322fe693ba9744c31bb4d04d489f503a
Urdiales Nicolas <nurdiales@e-biz.fr>
2017-04-20 17:00:56 +0200

    {DTO} creation des DTO et création des Mapper

--- src/main/java/fr/ebiz/nurdiales/trainingJava/dto/CompanyDTO.java
+++ src/main/java/fr/ebiz/nurdiales/trainingJava/dto/CompanyDTO.java
@@ -1,5 +1,22 @@
 package fr.ebiz.nurdiales.trainingJava.dto;
 
 public class CompanyDTO {
+    private Integer id;
+    private String name;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
