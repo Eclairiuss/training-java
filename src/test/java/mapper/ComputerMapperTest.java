ff3c4ba9a94011cb65ba5969d364a762b8d92676
Urdiales Nicolas <nurdiales@e-biz.fr>
2017-04-25 17:38:29 +0200

    {TEST} esquisse des tests pour la suite.

---
+++ src/test/java/mapper/ComputerMapperTest.java
@@ -0,0 +1,22 @@
+package fr.ebiz.nurdiales.trainingJava.mapper;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.sql.Date;
+
+public class ComputerMapperTest {
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+}
---
+++ src/test/java/servlet/SeleniumCreateComputer.java
@@ -0,0 +1,57 @@
+package servlet;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ebiz on 24/04/17.
+ */
+public class SeleniumCreateComputer {
+    private WebDriver driver;
+    private String baseUrl;
+    private JavascriptExecutor jse;
+
+    private static String ID_NAME = "computerName", ID_INTRODUCED = "introduced", ID_DISCONTINUED = "discontinued", ID_COMPANY = "companyId", ID_ADD = "add", ID_CANCEL = "cancel";
+
+    private WebElement name, introduced, discontinued, company, add, cancel;
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver");
+        driver = new FirefoxDriver();
+        baseUrl = "http://localhost:8080/add_computer";
+        jse = (JavascriptExecutor) driver;
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+        name=driver.findElement(By.id(ID_NAME));
+        introduced=driver.findElement(By.id(ID_INTRODUCED));
+        discontinued=driver.findElement(By.id(ID_DISCONTINUED));
+        company=driver.findElement(By.id(ID_COMPANY));
+        add=driver.findElement(By.id(ID_ADD));
+        cancel=driver.findElement(By.id(ID_CANCEL));
+    }
+
+    @Test
+    public void testAddFullComputer() throws Exception {
+        name.click();
+        name.sendKeys("nameTestSelenium");
+
+        Thread.sleep(100);
+
+        introduced.click();
+        introduced.sendKeys("1980-01-01");
+
+        Thread.sleep(100);
+
+        discontinued.click();
+        discontinued.sendKeys("1981-01-01");
+
+        Thread.sleep(100);
+        add.click();
+    }
+}
